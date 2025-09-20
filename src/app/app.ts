db4a26f7f006fcd2850411a93d7ed9e1759d4ba2
Fabrício Barros Cabral <fabriciofx@gmail.com>
2025-09-20 10:10:50 -0300

    Adiciona o Tutorial Projeto Sistema Bancário

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginTemplate } from "./pages/auth/login-template/login-template";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginTemplate],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
