64834bcc7e73227ecab274274c1c37f57d8e2b13
Fabrício Barros Cabral <fabriciofx@gmail.com>
2025-10-05 19:49:01 -0300

    Corrige formatação do código

--- src/app/shared/services/cliente/cliente-service.ts
+++ src/app/shared/services/cliente/cliente-service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { environment } from '../../../../environments/environment.development';
 import { Cliente } from '../../models/cliente';
-import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
