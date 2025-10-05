11922c124d2b95d57c9cce7481881d883452c686
Fabrício Barros Cabral <fabriciofx@gmail.com>
2025-10-05 19:04:35 -0300

    Configure rota padrão para /auth

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,15 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import {
+  provideClientHydration,
+  withEventReplay
+} from '@angular/platform-browser';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { routes } from './app.routes';
 import { authInterceptor } from './shared/interceptors/auth-interceptor';
 
 export const appConfig: ApplicationConfig = {
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,13 @@ import { Routes } from '@angular/router';
 import { CadastroCliente } from './pages/cliente/cadastro-cliente/cadastro-cliente';
 import { ListagemCliente } from './pages/cliente/listagem-cliente/listagem-cliente';
 import { LoginTemplate } from './pages/auth/login-template/login-template';
+import { CadastroConta } from './pages/conta/cadastro-conta/cadastro-conta';
 
 export const routes: Routes = [
+  {
+    path: '',
+    component: LoginTemplate,
+  },
   {
     path: 'auth',
     component: LoginTemplate,
@@ -26,7 +31,16 @@ export const routes: Routes = [
     ]
   },
   {
-    path: '',
-    component: ListagemCliente,
-  },
+    path: 'conta',
+    children: [
+      {
+        path: 'nova',
+        component: CadastroConta
+      },
+      {
+        path: 'editar/:id',
+        component: CadastroConta
+      }
+    ]
+  }
 ];
