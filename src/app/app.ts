784e28467eb289c567185f698f32260c88f13cb9
Fabrício Barros Cabral <fabriciofx@gmail.com>
2025-10-06 00:03:02 -0300

    Configura a Navbar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,26 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from "@angular/router";
+import { NavigationEnd, Router, RouterOutlet } from "@angular/router";
+import { CommonModule } from '@angular/common';
+import { Navbar } from './shared/components/navbar/navbar';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
+  private readonly router: Router;
   protected readonly title = signal('system-bank');
+  showNavbar = true;
+
+  constructor(router: Router) {
+    this.router = router;
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      this.showNavbar = event.url !== '/' && event.url !== '/auth';
+    });
+  }
 }
--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatNavList } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [MatSidenavModule, MatNavList, MatToolbarModule, MatIconModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss'
 })
 export class Navbar {
-
+  opened = false;
 }
