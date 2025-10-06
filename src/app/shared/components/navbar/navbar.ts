cef847177b4adb6b79fc9b7f3e950933b4c7e05d
Fabrício Barros Cabral <fabriciofx@gmail.com>
2025-10-06 11:46:39 -0300

    Corrige o navbar para abrir a tela toda e itens empilhados

--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -3,10 +3,11 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatNavList } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [MatSidenavModule, MatNavList, MatToolbarModule, MatIconModule],
+  imports: [MatSidenavModule, MatNavList, MatToolbarModule, MatIconModule, RouterOutlet],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss'
 })
