971cefa24c2cfeb2c1587e32cb31428e9646b234
Fabrício Barros Cabral <fabriciofx@gmail.com>
2025-10-07 22:17:33 -0300

    Adiciona Box, BoxOf e LateBox

---
+++ src/app/shared/box/box.ts
@@ -0,0 +1,47 @@
+export interface Box<T> {
+  store(item: T): T;
+  value(): T;
+}
+
+export class BoxOf<T> implements Box<T> {
+  private readonly content: T[];
+
+  constructor(item: T) {
+    this.content = [ item ];
+  }
+
+  store(item: T): T {
+    const old = this.content.pop()!;
+    this.content.push(item);
+    return old;
+  }
+
+  value(): T {
+    return this.content[0];
+  }
+}
+
+export class LateBox<T> implements Box<T> {
+  private readonly content: T[];
+
+  constructor() {
+    this.content = [];
+  }
+
+  store(item: T): T {
+    const old = this.content.pop();
+    if (!old) {
+      throw new Error("Box is empty!");
+    }
+    this.content.push(item);
+    return old;
+  }
+
+  value(): T {
+    const val = this.content[0];
+    if (!val) {
+      throw new Error("Box is empty!");
+    }
+    return val;
+  }
+}
