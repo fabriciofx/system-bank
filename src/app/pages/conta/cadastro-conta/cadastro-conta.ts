5f67cf50815899947175dfa3f7ad2a04aa032104
Fabrício Barros Cabral <fabriciofx@gmail.com>
2025-10-08 11:35:05 -0300

    Coloca todos os atributos como readonly no cadastro de contas

--- src/app/pages/conta/cadastro-conta/cadastro-conta.ts
+++ src/app/pages/conta/cadastro-conta/cadastro-conta.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ContaService } from '../../../shared/services/conta/conta-service';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatRadioModule } from '@angular/material/radio';
@@ -9,6 +9,7 @@ import { CommonModule } from '@angular/common';
 import Swal from 'sweetalert2';
 import { Conta } from '../../../shared/models/conta';
 import { InfiniteSelect } from "../../../shared/components/infinite-select/infinite-select";
+import { Box, BoxOf } from '../../../shared/box/box';
 
 @Component({
   selector: 'app-cadastro-conta',
@@ -25,14 +26,20 @@ import { InfiniteSelect } from "../../../shared/components/infinite-select/infin
   styleUrl: './cadastro-conta.scss'
 })
 export class CadastroConta {
+  private readonly router: Router;
+  private readonly route: ActivatedRoute;
+  private readonly contaService: ContaService;
   private readonly formGroup: FormGroup;
-  private editar: boolean;
+  private readonly editar: Box<boolean>;
 
   constructor(
-    private contaService: ContaService,
-    private router: Router,
-    private route: ActivatedRoute
+    router: Router,
+    route: ActivatedRoute,
+    contaService: ContaService
   ) {
+    this.router = router;
+    this.route = route;
+    this.contaService = contaService;
     this.formGroup = new FormGroup({
       id: new FormControl(null),
       cliente: new FormControl('', Validators.required),
@@ -40,12 +47,12 @@ export class CadastroConta {
       agencia: new FormControl('', Validators.required),
       saldo: new FormControl('', Validators.required)
     });
-    this.editar = false
+    this.editar = new BoxOf<boolean>(false);
   }
 
   ngOnInit(): void {
     if (this.route.snapshot.params["id"]) {
-      this.editar = true;
+      this.editar.store(true);
       this.contaService.pesquisaPorId(
         this.route.snapshot.params["id"]
       ).subscribe(
@@ -66,7 +73,7 @@ export class CadastroConta {
 
   cadastrar() {
     const conta: Conta = this.formGroup.value;
-    if (this.editar) {
+    if (this.editar.value()) {
       this.contaService.atualize(conta).subscribe({
         next: () => {
           Swal.fire({
