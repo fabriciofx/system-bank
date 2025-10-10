4fdcde217d5d5f19ede8d495d5048f9ee3009b36
Fabrício Barros Cabral <fabriciofx@gmail.com>
2025-10-10 11:37:58 -0300

    Corrige os tipos de retorno dos métodos de auth service

--- src/app/shared/services/auth/auth-service.ts
+++ src/app/shared/services/auth/auth-service.ts
@@ -5,6 +5,7 @@ import { Auth } from '../../models/auth';
 import { env } from '../../../../environments/env.dev';
 import { ErrorMessage } from '../../custom/message';
 import { ErrorReasons } from '../../custom/error-reasons';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -18,26 +19,24 @@ export class AuthService {
     this.router = router;
   }
 
-  login(auth: Auth) {
-    return this.http.post(`${env.API}/token/`, auth).subscribe(
-      {
-        next: (response) => {
-          localStorage.setItem('access_token', (response as any).access);
-          localStorage.setItem('refresh_token', (response as any).refresh);
-          this.router.navigate(['/cliente']);
-        },
-        error: (error) => {
-          new ErrorMessage(
-            'Oops...',
-            'Usuário e/ou senha inválidos!',
-            new ErrorReasons(error)
-          ).show();
-        }
+  login(auth: Auth): void {
+    this.http.post<Auth>(`${env.API}/token/`, auth).subscribe({
+      next: (response) => {
+        localStorage.setItem('access_token', (response as any).access);
+        localStorage.setItem('refresh_token', (response as any).refresh);
+        this.router.navigate(['/cliente']);
+      },
+      error: (error) => {
+        new ErrorMessage(
+          'Oops...',
+          'Usuário e/ou senha inválidos!',
+          new ErrorReasons(error)
+        ).show();
       }
-    );
+    });
   }
 
-  logout() {
+  logout(): void {
     if (typeof window !== 'undefined' && typeof localStorage !== 'undefined') {
       localStorage.removeItem('access_token');
       localStorage.removeItem('refresh_token');
@@ -45,21 +44,21 @@ export class AuthService {
     this.router.navigate(['/auth']);
   }
 
-  refreshToken(refresh: string) {
+  refreshToken(refresh: string): Observable<string> {
     return this.http.post<string>(`${env.API}/refresh/`, { refresh });
   }
 
-  getRefresh(): string | null {
+  getRefresh(): string {
     if (typeof window !== 'undefined' && typeof localStorage !== 'undefined') {
-      return localStorage.getItem('refresh_token');
+      return localStorage.getItem('refresh_token') || "";
     }
-    return null;
+    return "";
   }
 
-  getToken(): string | null {
+  getToken(): string {
     if (typeof window !== 'undefined' && typeof localStorage !== 'undefined') {
-      return localStorage.getItem('access_token');
+      return localStorage.getItem('access_token') || "";
     }
-    return null;
+    return "";
   }
 }
