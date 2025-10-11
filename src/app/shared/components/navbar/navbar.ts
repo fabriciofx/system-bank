d068f14a334754d9f907ae5770a8d5b21bc6c6b7
Fabrício Barros Cabral <fabriciofx@gmail.com>
2025-10-10 21:08:20 -0300

    Adiciona submenus ao menu principal

--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -5,6 +5,7 @@ import { MatNavList } from "@angular/material/list";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatIconModule } from "@angular/material/icon";
 import { MatButtonModule } from '@angular/material/button';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { YesNoMessage } from '../../custom/message';
 import { AuthService } from '../../services/auth/auth-service';
 
@@ -16,7 +17,8 @@ import { AuthService } from '../../services/auth/auth-service';
     MatNavList,
     MatToolbarModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatExpansionModule
   ],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss'
