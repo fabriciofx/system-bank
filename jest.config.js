cb67e5bcd14bef7d99aa86fe564007d7224599b0
Fabrício Barros Cabral <fabriciofx@gmail.com>
2025-10-11 12:10:38 -0300

    Configure o jest para executar em um projeto angular

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+const { createDefaultPreset } = require("ts-jest");
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+/** @type {import("jest").Config} **/
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  roots: ["<rootDir>/src/tests"],
+  moduleFileExtensions: ["ts", "js", "json"],
+  testMatch: ["**/*.test.ts"],
+  transform: {
+    "^.+\\.test.ts$": ["ts-jest", { tsconfig: "tsconfig.jest.json" }]
+  }
+};
