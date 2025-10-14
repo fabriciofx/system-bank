d02bcbf2732ee5b5e6af4a4b545a6202ccf6dc99
Fabrício Barros Cabral <fabriciofx@gmail.com>
2025-10-13 21:25:43 -0300

    Move a url do teste real do http core para uma variável

--- src/app/shared/core/http.real.spec.ts
+++ src/app/shared/core/http.real.spec.ts
@@ -5,6 +5,7 @@ import { provideHttpClient } from '@angular/common/http';
 import { Get } from './http';
 
 describe('http core', () => {
+  const url = 'https://aula-angular.bcorp.tec.br/api';
   let http: HttpClient;
 
   beforeEach(() => {
@@ -19,10 +20,7 @@ describe('http core', () => {
 
   it('should send a get request', () => {
     const msg = "As credenciais de autenticação não foram fornecidas.";
-    const response = new Get<{ detail: string }>(
-      http,
-      'https://aula-angular.bcorp.tec.br/api/token'
-    ).send();
+    const response = new Get<{ detail: string }>(http, `${url}/token`).send();
     response.value().subscribe(result => {
       expect(result.detail).toBe(msg);
     });
