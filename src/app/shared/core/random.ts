0d1e613657144df3ee6c4b8a40c0044167ea6a2d
Fabrício Barros Cabral <fabriciofx@gmail.com>
2025-10-17 18:04:38 -0300

    Adiciona Random e RandomString que gera strings aleatórias de certo tamanho

---
+++ src/app/shared/core/random.ts
@@ -0,0 +1,19 @@
+export interface Random<T> {
+  value(): T;
+}
+
+export class RandomString implements Random<string> {
+  private readonly length: number;
+
+  constructor(length: number) {
+    this.length = length;
+  }
+
+  value(): string {
+    // eslint-disable-next-line max-len
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const array = new Uint8Array(this.length);
+    crypto.getRandomValues(array);
+    return Array.from(array, byte => chars[byte % chars.length]).join('');
+  }
+}
